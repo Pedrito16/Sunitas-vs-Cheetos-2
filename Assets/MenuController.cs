4c1a853048257179aab2536d711af2ca61f2f961
JosueRFP <josue.petri.senac@gmail.com>
2024-07-31 11:39:14 -0300

    Botoes

---
+++ Assets/MenuController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject creditsPainel;
+
+    public void Teleport(string tp)
+    {
+            SceneManager.LoadScene(tp);
+    }
+    public void OpenCreditsBTN()
+    {
+        creditsPainel.SetActive(true);
+    }
+    public void CloseCreditsBTN()
+    {
+        creditsPainel.SetActive(false);
+    }
+    public void QuitBTN()
+    {
+        Application.Quit();
+        Debug.Log("Quitou");
+    }
+}
