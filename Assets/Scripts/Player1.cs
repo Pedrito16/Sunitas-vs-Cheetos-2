ea74c2af803900347a8fb0ff73d26a0792bb6261
Pedrito16 <pedrogabrielvicente98@gmail.com>
2024-08-01 09:37:44 -0300

    arrumando rotações

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public float horizontal;
+    public Rigidbody2D body;
+    public GameObject bullet;
+    public float speed = 5, jumpStrength = 5, bulletSpeed = 8;
+    Collider2D footCollision;
+    bool groundCheck;
+    public Transform foot;
+    public int direction = -1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
+
+        Vector2 scale = bullet.transform.localScale;
+        scale.x = direction;
+        bullet.transform.localScale = scale;
+
+        footCollision = Physics2D.OverlapCircle(foot.position, 0.05f);
+        groundCheck = footCollision;
+        if (Input.GetButtonDown("Vertical") && groundCheck)
+        {
+            body.AddForce(new Vector2(0, jumpStrength * 100));
+
+        }
+        if (horizontal != 0)
+        {
+            direction = (int)horizontal;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject temp = Instantiate(bullet, transform.position, foot.rotation);
+            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * direction, 0);
+        }
+    }
+}
+
+
--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player1 : MonoBehaviour
+public class Player2 : MonoBehaviour
 {
     public float horizontal;
     public Rigidbody2D body;
@@ -12,7 +12,7 @@ public class Player1 : MonoBehaviour
     bool groundCheck;
     public Transform foot;
     public int direction = -1;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,27 +22,25 @@ public class Player1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal2");
         body.velocity = new Vector2(horizontal * speed, body.velocity.y);
 
 
         footCollision = Physics2D.OverlapCircle(foot.position, 0.05f);
         groundCheck = footCollision;
-        if (Input.GetButtonDown("Vertical") && groundCheck)
+        if (Input.GetButtonDown("Vertical2") && groundCheck)
         {
             body.AddForce(new Vector2(0, jumpStrength * 100));
-
+            print("apertado");
         }
         if (horizontal != 0)
         {
             direction = (int)horizontal;
         }
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire2"))
         {
             GameObject temp = Instantiate(bullet, transform.position, foot.rotation);
             temp.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * direction, 0);
         }
     }
 }
-
-
